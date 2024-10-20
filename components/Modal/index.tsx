20e01d70318bcf703077a53395bec82c84a86189
ayush_tibra <ayush.tibra@growisto.com>
2024-10-20 12:55:03 +0530

    custom modal component usinf tailwind

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,11 +1,80 @@
-const Modal = () => {
-    return (
-        <div className="modal-overlay fixed w-full h-full top-0 left-0 bg-[rgba(0,0,0,0.4)]">
-            <div className="modal-content absolute top-1/2 left-1/2 translate-x-[-1/2] translate-y-[-1/2]">
-
-            </div>
-        </div>
-    )
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+interface ModalProps {
+    isOpen: boolean;
+    allowScroll?: boolean;
+    className?: string;
+    outsideClickModalClose?: boolean;
+    handleClose?(): void;
+    children: React.ReactNode;
 }
+
+const Modal = ({ 
+    isOpen, 
+    allowScroll = true, 
+    className = '', 
+    handleClose, 
+    outsideClickModalClose = false, 
+    children 
+}: ModalProps) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const outsideClickHandler = (e: MouseEvent) => {            
+            // Check if the click is outside the modal content
+            if (ref.current && !ref.current.contains(e.target as Node)) {                
+                if (handleClose) {
+                    handleClose(); // Call the handleClose function
+                } else{
+                    console.log('Please provide close handle function or see docs')
+                }
+            }
+        };
+
+        let timer: NodeJS.Timeout;
+        if (isOpen && outsideClickModalClose) {
+            timer = setTimeout(() => {
+                window.addEventListener('click', outsideClickHandler);
+            }, 100); // Delay the event listener by 100ms - so that when modal open through button it'll not trigger
+        }
+
+        return () => {
+            clearTimeout(timer); // Clear the timeout if component unmounts
+            window.removeEventListener('click', outsideClickHandler);
+        };
+    }, [isOpen, handleClose, outsideClickModalClose]);
+
+    // Stop scrolls of body when modal opens
+    useEffect(() => {
+        if(isOpen && !allowScroll){
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = 'auto'
+        }
+
+        return () => {
+            document.body.style.overflow = 'auto'
+        }
+    },[isOpen, allowScroll])
+
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <>
+            {createPortal(
+                <div className={`${className} modal-overlay fixed w-full h-full top-0 left-0 bg-[rgba(255,255,255,0.2)] overflow-auto`}>
+                    <div 
+                        ref={ref} 
+                        className="modal-content w-full max-w-[80%] lg:max-w-[400px] absolute bg-white text-black p-4 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] overflow-auto">
+                        {children}
+                    </div>
+                </div>,
+                document.getElementById('myportal')!
+            )}
+        </>
+    );
+};
 
 export default Modal;
--- pages/custom-modal/index.tsx
+++ pages/custom-modal/index.tsx
@@ -1,6 +1,71 @@
+import Modal from "@/components/Modal";
+import { useState } from "react";
+import { XMarkIcon } from '@heroicons/react/24/outline'
+
 const CustomModal = () => {
-    return(
-        <>Modal Component</>
+    const [open, setOpen] = useState(false);
+
+    const openModal = (e: React.MouseEvent) => {
+        // e.stopPropagation(); // Prevent event from bubbling to window - or add a timer in modal component
+        setOpen(true);
+      };
+
+    const handleModalState = () => {
+        setOpen(false);
+    }
+
+    return (
+        <div className="flex flex-col justify-center text-center p-4">
+            <p>Modal Component</p>
+            <button onClick={openModal} className="py-4">Open Modal</button>
+            <Modal
+                isOpen={open}
+                allowScroll={false}
+                outsideClickModalClose={true}
+                handleClose={handleModalState}
+                className='customClass'
+            >
+                <div className="modal">
+                    <div className="absolute right-0 top-0 pr-2 pt-4 sm:block">
+                        <button
+                            type="button"
+                            onClick={handleModalState}
+                            className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                            <span className="sr-only">Close</span>
+                            <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+                        </button>
+                    </div>
+                    <div className="modal-header border-b border-solid border-gray mb-4 flex justify-between pb-2">
+                        <span>Modal Header</span>
+                    </div>
+                    <div className="base-content max-w-[400px]">
+                        <p>Modal content written here</p>
+                    </div>
+                    <div className="btn-group flex justify-between gap-5 mt-4">
+                        <button
+                            type="button"
+                            onClick={handleModalState}
+                            className="w-[48%] rounded bg-white p-3 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
+                            Close
+                        </button>
+                        <button
+                            type="button"
+                            className="w-[48%] rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Continue
+                        </button>
+                    </div>
+                </div>
+            </Modal>
+            <p>Modal Component Modal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal Component</p>
+            <p>Modal Component Modal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal Component</p>
+            <p>Modal Component Modal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal Component</p>
+            <p>Modal Component Modal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal Component</p>
+            <p>Modal Component Modal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal Component</p>
+            <p>Modal Component Modal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal ComponentModal Component</p>
+        </div>
     )
 }
 
